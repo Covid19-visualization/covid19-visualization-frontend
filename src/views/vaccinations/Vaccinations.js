abedc266efe19073b0a256d01234cd9bd97ad273
valecor95 <coretti.1635747@studenti.uniroma1.it>
2021-09-05 13:49:15 +0200

    Fix radar chart

--- src/views/vaccinations/Vaccinations.js
+++ src/views/vaccinations/Vaccinations.js
@@ -20,57 +20,64 @@ const Vaccinations = () => {
 
 
 
-    const data3 = [
-        [//iPhone
-          {axis:"Battery Life",value:0.22},
-          {axis:"Brand",value:0.28},
-          {axis:"Contract Cost",value:0.29},
-          {axis:"Design And Quality",value:0.17},
-          {axis:"Have Internet Connectivity",value:0.22},
-          {axis:"Large Screen",value:0.02},
-          {axis:"Price Of Device",value:0.21},
-          {axis:"To Be A Smartphone",value:0.50}			
-        ],[//Samsung
-          {axis:"Battery Life",value:0.27},
-          {axis:"Brand",value:0.16},
-          {axis:"Contract Cost",value:0.35},
-          {axis:"Design And Quality",value:0.13},
-          {axis:"Have Internet Connectivity",value:0.20},
-          {axis:"Large Screen",value:0.13},
-          {axis:"Price Of Device",value:0.35},
-          {axis:"To Be A Smartphone",value:0.38}
-        ],[//Nokia Smartphone
-          {axis:"Battery Life",value:0.26},
-          {axis:"Brand",value:0.10},
-          {axis:"Contract Cost",value:0.30},
-          {axis:"Design And Quality",value:0.14},
-          {axis:"Have Internet Connectivity",value:0.22},
-          {axis:"Large Screen",value:0.04},
-          {axis:"Price Of Device",value:0.41},
-          {axis:"To Be A Smartphone",value:0.30}
-        ]
+    //Data
+    var d = [
+      [
+          {axis:"Email",value:0.59},
+          {axis:"Social Networks",value:0.56},
+          {axis:"Internet Banking",value:0.42},
+          {axis:"News Sportsites",value:0.34},
+          {axis:"Search Engine",value:0.48},
+          {axis:"View Shopping sites",value:0.14},
+          {axis:"Paying Online",value:0.11},
+          {axis:"Buy Online",value:0.05},
+          {axis:"Stream Music",value:0.07},
+          {axis:"Online Gaming",value:0.12},
+          {axis:"Navigation",value:0.27},
+          {axis:"App connected to TV program",value:0.03},
+          {axis:"Offline Gaming",value:0.12},
+          {axis:"Photo Video",value:0.4},
+          {axis:"Reading",value:0.03},
+          {axis:"Listen Music",value:0.22},
+          {axis:"Watch TV",value:0.03},
+          {axis:"TV Movies Streaming",value:0.03},
+          {axis:"Listen Radio",value:0.07},
+          {axis:"Sending Money",value:0.18},
+          {axis:"Other",value:0.07},
+          {axis:"Use less Once week",value:0.08}
+      ],[
+          {axis:"Email",value:0.48},
+          {axis:"Social Networks",value:0.41},
+          {axis:"Internet Banking",value:0.27},
+          {axis:"News Sportsites",value:0.28},
+          {axis:"Search Engine",value:0.46},
+          {axis:"View Shopping sites",value:0.29},
+          {axis:"Paying Online",value:0.11},
+          {axis:"Buy Online",value:0.14},
+          {axis:"Stream Music",value:0.05},
+          {axis:"Online Gaming",value:0.19},
+          {axis:"Navigation",value:0.14},
+          {axis:"App connected to TV program",value:0.06},
+          {axis:"Offline Gaming",value:0.24},
+          {axis:"Photo Video",value:0.17},
+          {axis:"Reading",value:0.15},
+          {axis:"Listen Music",value:0.12},
+          {axis:"Watch TV",value:0.1},
+          {axis:"TV Movies Streaming",value:0.14},
+          {axis:"Listen Radio",value:0.06},
+          {axis:"Sending Money",value:0.16},
+          {axis:"Other",value:0.07},
+          {axis:"Use less Once week",value:0.17}
+      ]
       ];
-    
-    var margin = {top: 100, right: 100, bottom: 100, left: 100};
-
-    var radarChartOptions = {
-        w: width,
-        h: height,
-        margin: margin,
-        maxValue: 0.5,
-        levels: 5,
-        roundStrokes: true,
-      };
-
-
-    
+        
     return (
         <>
             <div className={"vaccination-container"} style={{ width: "100%", height: "100%", display: "flex", backgroundColor: debug ? "beige" : null }} >
                 <div className={"first-half-container"} style={{ width: "50%", height: "100%", backgroundColor: debug ? "green" : null, flex: 0.5 }}>
                     <div className={"first-component"} style={{ width: "100%", height: "60%", backgroundColor: debug ? "blue" : null }} />
                     <div className={"second-component"} style={{ width: "100%", height: "40%", backgroundColor: debug ? "brown" : null }} />
-                      <RadarChart id={".radarChart"} data={data3} options={radarChartOptions} />
+                      <RadarChart data={data3} />
                 </div>
                 <div className={"second-half-container"} style={{ width: "50%", height: "100%", backgroundColor: debug ? "green" : null, flex: 0.5 }}>
                     <div className={"first-component"} style={{ width: "100%", height: "50%", backgroundColor: debug ? "yellow" : null, justifyContent: "center", alignItems: "center" }}>
